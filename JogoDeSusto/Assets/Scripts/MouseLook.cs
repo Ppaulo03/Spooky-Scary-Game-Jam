c7eb618a5b715581227a9ebb4f815f2eec3aa844
Pedro Paulo Carvalho <ppaulo030601@gmail.com>
2020-10-27 00:33:13 -0300

    ProBuilder/PlayerController/Lights
    *Added Probuilder Asset
    *Created a prototype of Player Controller - super basic, but can walk and use stairs
    *Played a little with the use of the light

---
+++ JogoDeSusto/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform body = null;
+
+    [SerializeField]
+    private float sensitivity = 100f, maxY = 90f, minY = -90f;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void FixedUpdate()
+    {
+        //X rotation SetUp (Left/Right)
+        body.Rotate(Vector3.up* Input.GetAxis("Mouse X") * sensitivity);
+
+        //Y rotation SetUp (Up/Down)
+        xRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+        xRotation = Mathf.Clamp(xRotation, minY, maxY);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    } 
+}
---
+++ JogoDeSusto/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller = null;
+
+    [SerializeField]
+    private Transform groundCheck = null;
+
+    [SerializeField]
+    private LayerMask groundMask = ~0;
+
+    [SerializeField]
+    private float speed = 12f, gravity = -9.81f, groundDistance = 0.4f;
+
+    private Vector3 velocity;
+
+    void Update()
+    {
+        if(Physics.CheckSphere(groundCheck.position, groundDistance, groundMask)) velocity.y = -2f;
+        else velocity.y += gravity*Time.deltaTime;
+        controller.Move(velocity*Time.deltaTime);
+
+        Vector3 move = transform.right*Input.GetAxis("Horizontal") + transform.forward*Input.GetAxis("Vertical");
+        controller.Move(move*speed*Time.deltaTime);
+    }
+}
