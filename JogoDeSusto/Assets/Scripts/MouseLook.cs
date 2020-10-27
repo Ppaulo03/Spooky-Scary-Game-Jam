637ba93d1a37246bc0c2ec934f7fae4e75e248e0
Pedro Paulo Carvalho <ppaulo030601@gmail.com>
2020-10-27 07:54:00 -0300

    A little more of lights

--- JogoDeSusto/Assets/Scripts/MouseLook.cs
+++ JogoDeSusto/Assets/Scripts/MouseLook.cs
@@ -9,22 +9,28 @@ public class MouseLook : MonoBehaviour
     private Transform body = null;
 
     [SerializeField]
-    private float sensitivity = 100f, maxY = 90f, minY = -90f;
-    private float xRotation = 0f;
+    private float sensitivity = 2.5f, maxY = 90f, minY = -90f;
+    private float xRotation = 0f, yrotationOverClamp;
+
+    [SerializeField]
+    private bool  rotateX = false, rotateY = false;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        xRotation = transform.rotation.eulerAngles.x;
     }
 
     void FixedUpdate()
     {
-        //X rotation SetUp (Left/Right)
-        body.Rotate(Vector3.up* Input.GetAxis("Mouse X") * sensitivity);
-
-        //Y rotation SetUp (Up/Down)
-        xRotation -= Input.GetAxis("Mouse Y") * sensitivity;
-        xRotation = Mathf.Clamp(xRotation, minY, maxY);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        if(rotateX) //X rotation SetUp (Left/Right)
+            body.Rotate(Vector3.up* Input.GetAxis("Mouse X") * sensitivity);
+        
+        if(rotateY){ //Y rotation SetUp (Up/Down)
+            xRotation -= Input.GetAxis("Mouse Y") * sensitivity;
+            xRotation = Mathf.Clamp(xRotation, minY, maxY);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
     } 
+
 }
