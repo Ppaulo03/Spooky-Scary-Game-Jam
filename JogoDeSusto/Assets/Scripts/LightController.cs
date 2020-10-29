9177d514dafd68239bb5a0763c4f7132619a8a29
Pedro Paulo Carvalho <ppaulo030601@gmail.com>
2020-10-29 08:05:49 -0300

    Ajustes

--- JogoDeSusto/Assets/Scripts/LightController.cs
+++ JogoDeSusto/Assets/Scripts/LightController.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
-    [SerializeField] private Material ligthOn = null, LightOff = null;
-    [SerializeField] private GameObject lightEffect = null;
+    [SerializeField] private Material ligthOn = null, lightOff = null;
     [SerializeField] private bool isOn = false;
+    private Light lightEffect;
+    private MeshRenderer meshRenderer;
+    private Material[] matArray;
+    private void Start() {
+        
+        lightEffect = GetComponent<Light>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        matArray = meshRenderer.materials;
+        isOn = !isOn;
+        Turn();
+    }
 
     public void Turn(){
         isOn = !isOn;
-        lightEffect.SetActive(isOn);
-        if(isOn) GetComponent<MeshRenderer> ().material = ligthOn;
-        else GetComponent<MeshRenderer> ().material = LightOff;
+        lightEffect.enabled = isOn;
+        if(isOn) matArray[1] = ligthOn;
+        else matArray[1] = lightOff;
+        meshRenderer.materials = matArray;
     }
+
+    public void Flicker(){
+        if(isOn) StartCoroutine(FlickerCo());
+    }
+    private IEnumerator FlickerCo(){
+        float originalLum = lightEffect.intensity;
+        float brightness = originalLum;
+        for(int i = 0; i < 5; i++){
+
+            lightEffect.intensity = Random.Range(lightEffect.intensity/1.25f, lightEffect.intensity/4.75f);
+            matArray[1] = lightOff;
+            meshRenderer.materials = matArray;
+            yield return new WaitForSeconds(Random.Range(0.1f,0.5f));
+
+            lightEffect.intensity = Random.Range(lightEffect.intensity*1.25f, lightEffect.intensity*2f);
+            matArray[1] = ligthOn;
+            meshRenderer.materials = matArray;
+            yield return new WaitForSeconds(Random.Range(0.1f,0.5f));
+
+        }
+        lightEffect.intensity = originalLum;
+    }
+
 }
